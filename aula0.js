41ab9dae2f53d43e4d04e48fe276d6cac311c5b3
PancinhaBR <eduardopaularosa@gmail.com>
2025-07-28 09:49:57 -0300

    aufgdabsjkdvah

---
+++ aula0.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("digite o seu nome:");
+let idade = prompt("digite sua idade:");
+console.log("Bem Vindo ",nome,"você tem ",idade,"Anos ");
