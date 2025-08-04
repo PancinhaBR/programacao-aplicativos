a113a2d10b6881c380436d57b42206183ee82817
skibidi guy <pancinha@gmail.comahsh>
2025-08-04 11:24:08 -0300

    3

---
+++ laços-de-repetição1.js
@@ -0,0 +1,8 @@
+let resultado = 0
+
+for(let tabuada0 = 2; tabuada0 <10; tabuada0++){
+   for(let tabuada1 = 0; tabuada1 <= 10; tabuada1++){
+    resultado = tabuada0 * tabuada1;
+    console.log(tabuada0,"*",tabuada1,"=",resultado);
+   }
+}
